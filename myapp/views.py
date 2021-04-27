e7b40837fec567f11ce8c7fea5b829aee2220af7
Damilola <damrichee@yahoo.com>
2021-04-27 23:03:59 +0100

    Django blog

--- myapp/views.py
+++ myapp/views.py
@@ -4,3 +4,7 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return HttpResponse("Welcome Damilola")
+
+#User Register
+def Register(request):
+    render(request, 'registration/register.html')
